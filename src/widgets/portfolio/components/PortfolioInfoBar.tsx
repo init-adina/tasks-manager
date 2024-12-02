367de3db494480028c229a43d78b1e4fb4893dac
Adina Meiramkhanova <adina@init.red>
2024-12-02 14:19:19 +0500

    fix: portfolioInfoBar

--- src/widgets/portfolio/components/PortfolioInfoBar.tsx
+++ src/widgets/portfolio/components/PortfolioInfoBar.tsx
@@ -1,37 +1,45 @@
+import { useAuth } from "src/core/providers/AuthProvider";
+
 function PortfolioInfoBar() {
+  const { user, loading: isLoading } = useAuth();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (!user) return <div>Not authorized</div>;
+
   return (
     <div className="portfolio-info-bar border shadow-md rounded-md lg:h-[22rem] p-6 flex flex-col gap-12">
       <div className="top grid grid-cols-1 sm:grid-cols-3 gap-12">
-        <div className="flex flex-col gap-2 text-sm pb-2 border-b">
+        <div className="gender flex flex-col gap-2 text-sm pb-2 border-b">
           <div className="category text-gray-600">Gender</div>
-          <div>Female</div>
+          <div>{user.gender}</div>
         </div>
 
         <div className="flex flex-col gap-2 text-sm pb-2 border-b">
           <div className="category text-gray-600">Birthday</div>
-          <div>January 15, 2004</div>
+          <div>{user.birth_date_at || "not defined"}</div>
         </div>
 
         <div className="flex flex-col gap-2 text-sm pb-2 border-b">
           <div className="category text-gray-600">Phone Number</div>
-          <div>7 (777) 777 77 77</div>
+          <div>{user.phone || "not defined"}</div>
         </div>
       </div>
 
       <div className="middle grid grid-cols-1 sm:grid-cols-3 gap-12">
         <div className="flex flex-col gap-2 text-sm pb-2 border-b">
           <div className="category text-gray-600">Street Address</div>
-          <div>address place</div>
+          <div>{user.address || "not defined"}</div>
+        </div>
+
+        <div className="role flex flex-col gap-2 text-sm pb-2 border-b">
+          <div className="category text-gray-600">Role</div>
+          <div>{user.position.name}</div>
         </div>
 
         <div className="flex flex-col gap-2 text-sm pb-2 border-b">
-          <div className="category text-gray-600">City</div>
-          <div>Almaty</div>
-        </div>
-
-        <div className="flex flex-col gap-2 text-sm pb-2 border-b">
-          <div className="category text-gray-600">Phone Number</div>
-          <div>7 (777) 777 77 77</div>
+          <div className="category text-gray-600">Registered Date</div>
+          <div>June 16, 2024</div>
         </div>
       </div>
 
@@ -39,11 +47,6 @@ function PortfolioInfoBar() {
         <div className="flex flex-col gap-2 text-sm pb-2 border-b">
           <div className="category text-gray-600">Member Status</div>
           <div>Active</div>
-        </div>
-
-        <div className="flex flex-col gap-2 text-sm pb-2 border-b">
-          <div className="category text-gray-600">Registered Date</div>
-          <div>June 16, 2024</div>
         </div>
       </div>
     </div>
